3cc0cdf3a23f258177bda98a84af3d3f59746f19
Stephan Uhle <stephanu@gmx.net>
2023-12-04 22:56:59 +0000

    Fixed build errors.

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -24,7 +24,10 @@ from .const import MOCK_CONFIG
 @pytest.fixture(autouse=True)
 def bypass_setup_fixture():
     """Prevent setup."""
-    with patch("custom_components.mygekko.async_setup", return_value=True,), patch(
+    with patch(
+        "custom_components.mygekko.async_setup",
+        return_value=True,
+    ), patch(
         "custom_components.mygekko.async_setup_entry",
         return_value=True,
     ):
